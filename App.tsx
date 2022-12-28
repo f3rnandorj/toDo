e14f0e1a9532a121460224c35e776f0f23cafc9f
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-12-27 21:20:56 -0300

    form and submit styles

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from "react-native";
-import { SafeAreaView } from "react-native";
 import { Home } from "./src/screens/Home";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
     <>
       <StatusBar backgroundColor="#0d0d0d" barStyle="light-content" />
-      <Home />
+      {fontLoaded ? <Home /> : null}
     </>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import { Container, AddToDo, Input, SendButton } from "./styles";
+import { Ionicons } from "@expo/vector-icons";
 
 export function Home() {
   return (
     <Container>
       <Header />
+      <AddToDo>
+        <Input placeholder="Adicione uma nova tarefa" />
+        <SendButton onPress={() => {}}>
+          <Ionicons name="add-circle-outline" size={24} color="#f2f2f2" />
+        </SendButton>
+      </AddToDo>
     </Container>
   );
 }
